481974ab8f43fffea4bf056fb0cdc3d7cd296902
mache102 <bee365go@gmail.com>
2024-07-26 17:10:35 +0800

    use gl.ONE for source to fix outline white blurs

--- src/main.cpp
+++ src/main.cpp
@@ -406,7 +406,8 @@ bool init_opengl() {
 
   glEnable(GL_BLEND);
   glEnable(GL_MULTISAMPLE);
-  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+  // glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+  glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
 
   // no vsync
   glfwSwapInterval(vsync);
